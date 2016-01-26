ad29aa546369aa304e5dbf143dfe423dbe672148
John D. Gaffney <john.gaffney@isobar.com>
2016-01-26 16:37:06 -0500

    removing some erroneously created models and controllers

--- config/routes.js
+++ config/routes.js
@@ -32,10 +32,13 @@ module.exports.routes = {
   *                                                                          *
   ***************************************************************************/
 
-  '/': {
-    view: 'homepage'
-  }
-
+  '/': { view: 'homepage' },
+  'get /login': { view: 'user/login' },
+  'get /signup': { view: 'user/signup' },
+  '/welcome': { view: 'user/welcome' },
+  'post /login': 'UserController.login',
+  'post /signup': 'UserController.signup',
+  '/logout': 'UserController.logout'
   /***************************************************************************
   *                                                                          *
   * Custom routes here...                                                    *
